ddc6a69a9019dea56502d63bd0f048058f88087f
vot-blin <xmedli@bk.ru>
2025-03-19 11:36:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Склонируйте репозиторий:
+   ```bash
+   git clone https://github.com/ваш-username/ваш-репозиторий.git
+   cd ваш-репозиторий
+2. Запустите приложение с помощью Docker Compose:
+    docker-compose up --build
+3. Откройте браузер и перейдите по адресу:
+    http://localhost:3000
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import './component/TodoList.js';
+import TodoList from './component/TodoList.js';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        123
+      </header>
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
